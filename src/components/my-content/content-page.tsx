5983d7df0105b594ccd85cd5ef85fdc0bd934c31
Adriano Lima <adrianulima@hotmail.com>
2024-12-31 04:02:15 -0300

    Refactored content pages to reuse logic

---
+++ src/components/my-content/content-page.tsx
@@ -0,0 +1,115 @@
+import useDebounce from "@/hooks/useDebounce";
+import { TContent } from "@/ir-data/utils/types";
+import { Flex } from "@chakra-ui/react";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import ContentFilterPanel from "./content-filter-panel";
+import ContentHeader from "./content-header";
+import ContentTable from "./content-table";
+import ContentTableRow from "./content-table-row";
+
+function ContentPage({
+  allTab,
+  content,
+  contentListJson,
+  description,
+  favorites,
+  freeCount,
+  infoUrl,
+  myContent,
+  skuIcon,
+  tabs,
+  title,
+  wish,
+}: {
+  allTab: string;
+  content: "cars" | "tracks";
+  contentListJson: TContent[];
+  description: string;
+  favorites: number[];
+  freeCount: number;
+  infoUrl: (id: number) => string;
+  myContent: number[];
+  skuIcon: IconDefinition;
+  tabs: { [key: string]: string };
+  title: string;
+  wish: number[];
+}) {
+  const [tabCategory, setTabCategory] = useState<string>(allTab);
+  const [pop, setOpenPop] = useState<number>(-1);
+  const [search, setSearch] = useState<string>("");
+  const [list, setList] = useState(contentListJson);
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  useEffect(() => {
+    const filteredContent =
+      tabCategory === allTab
+        ? contentListJson
+        : contentListJson.filter((content: any) =>
+            content.categories.some((v: string) => tabs[v] === tabCategory),
+          );
+    const trimmedSearch = debouncedSearch
+      .trim()
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    const filteredContentWithSearch = !!trimmedSearch
+      ? filteredContent.filter((content) =>
+          content.name
+            .toLowerCase()
+            .normalize("NFD")
+            .replace(/[\u0300-\u036f]/g, "")
+            .includes(trimmedSearch),
+        )
+      : filteredContent;
+
+    setList(filteredContentWithSearch);
+  }, [debouncedSearch, tabCategory]);
+
+  return (
+    <Flex direction="column" height="100%" width="100%" gap="8px">
+      <ContentHeader
+        freeCount={freeCount}
+        ownedCount={myContent.length ?? 0}
+        wishCount={wish.length ?? 0}
+        title={title}
+        description={description}
+      />
+
+      <ContentFilterPanel
+        tabs={tabs}
+        tab={tabCategory}
+        onTabChange={setTabCategory}
+        onSearchChange={setSearch}
+      />
+
+      <ContentTable
+        list={list}
+        children={(item) => (
+          <ContentTableRow
+            key={item.id}
+            content={content}
+            infoUrl={infoUrl(item.id)}
+            skuIcon={skuIcon}
+            id={item.id}
+            price={item.price}
+            name={item.name}
+            logo={item.logo}
+            categories={item.categories}
+            free={item.free}
+            skuGroup={item.skuGroup ? Object.values(item.skuGroup) : undefined}
+            series={item.series}
+            popOpen={pop === item.id}
+            setOpenPop={setOpenPop}
+            owned={myContent.includes(item.id)}
+            favorite={favorites.includes(item.id)}
+            wish={wish.includes(item.id)}
+          />
+        )}
+      />
+    </Flex>
+  );
+}
+
+export default ContentPage;
--- src/components/my-content/tracks-page.tsx
+++ src/components/my-content/tracks-page.tsx
@@ -1,101 +1,29 @@
-import useDebounce from "@/hooks/useDebounce";
 import SORTED_TRACKS, { FREE_TRACKS } from "@/ir-data/utils/tracks";
 import { ETrackCategories } from "@/ir-data/utils/types";
 import { IR_URL } from "@/ir-data/utils/urls";
 import { useIr } from "@/store/ir";
-import { Flex } from "@chakra-ui/react";
 import { faRoad } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
-import ContentFilterPanel from "./content-filter-panel";
-import ContentHeader from "./content-header";
-import ContentTable from "./content-table";
-import ContentTableRow from "./content-table-row";
+import ContentPage from "./content-page";
 
 function TracksPage() {
-  const [tabCategory, setTabCategory] = useState<ETrackCategories>(
-    ETrackCategories.all,
-  );
-  const [pop, setOpenPop] = useState<number>(-1);
-  const [search, setSearch] = useState<string>("");
-  const [list, setList] = useState(SORTED_TRACKS);
-
   const { myTracks, wishTracks, favoriteTracks } = useIr();
-
-  const debouncedSearch = useDebounce(search, 500);
-
-  useEffect(() => {
-    const filteredCars =
-      tabCategory === ETrackCategories.all
-        ? SORTED_TRACKS
-        : SORTED_TRACKS.filter((track) =>
-            track.categories.some(
-              (v: string) =>
-                ETrackCategories[v as keyof typeof ETrackCategories] ===
-                tabCategory,
-            ),
-          );
-    const trimmedSearch = debouncedSearch
-      .trim()
-      .toLowerCase()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "");
-    const filteredCarsWithSearch = !!trimmedSearch
-      ? filteredCars.filter((car) =>
-          car.name
-            .toLowerCase()
-            .normalize("NFD")
-            .replace(/[\u0300-\u036f]/g, "")
-            .includes(trimmedSearch),
-        )
-      : filteredCars;
-
-    setList(filteredCarsWithSearch);
-  }, [debouncedSearch, tabCategory]);
-
   return (
-    <Flex direction="column" height="100%" width="100%" gap="8px">
-      <ContentHeader
-        freeCount={FREE_TRACKS}
-        ownedCount={myTracks.length ?? 0}
-        wishCount={wishTracks.length ?? 0}
-        title={"My Tracks"}
-        description={
-          "Mark the tracks you own and select your favorites. Use the wishlist to preview the content before you buy it."
-        }
-      />
-
-      <ContentFilterPanel
-        tabs={ETrackCategories}
-        tab={tabCategory}
-        onTabChange={setTabCategory}
-        onSearchChange={setSearch}
-      />
-
-      <ContentTable
-        list={list}
-        children={(item) => (
-          <ContentTableRow
-            key={item.id}
-            content="tracks"
-            infoUrl={`${IR_URL.members}/shop/tracks?trackId=${item.id}`}
-            skuIcon={faRoad}
-            id={item.id}
-            price={item.price}
-            name={item.name}
-            logo={item.logo}
-            categories={item.categories}
-            free={item.free}
-            skuGroup={item.skuGroup ? Object.values(item.skuGroup) : undefined}
-            series={item.series}
-            popOpen={pop === item.id}
-            setOpenPop={setOpenPop}
-            owned={myTracks.includes(item.id)}
-            favorite={favoriteTracks.includes(item.id)}
-            wish={wishTracks.includes(item.id)}
-          />
-        )}
-      />
-    </Flex>
+    <ContentPage
+      allTab={ETrackCategories.all}
+      content={"tracks"}
+      contentListJson={SORTED_TRACKS}
+      description={
+        "Mark the tracks you own and select your favorites. Use the wishlist to preview the content before you buy it."
+      }
+      favorites={favoriteTracks}
+      freeCount={FREE_TRACKS}
+      infoUrl={(id) => `${IR_URL.members}/shop/tracks?trackId=${id}`}
+      myContent={myTracks}
+      skuIcon={faRoad}
+      tabs={ETrackCategories}
+      title={"My Tracks"}
+      wish={wishTracks}
+    />
   );
 }
 
