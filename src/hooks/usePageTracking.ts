0d7a2056210a497f9844f360cb50e16b9d0f3a0c
Adriano Lima <adrianulima@hotmail.com>
2025-01-20 17:01:29 -0300

    Added screen_name to page_view event

--- src/hooks/usePageTracking.ts
+++ src/hooks/usePageTracking.ts
@@ -1,5 +1,11 @@
+import { ETabs } from "@/store/ui";
 import { useEffect } from "react";
 import { useLocation, useSearch } from "wouter";
+
+function getKeyFromValue(value: string): string | undefined {
+  const entry = Object.entries(ETabs).find(([_, val]) => val === value);
+  return entry ? entry[0] : undefined;
+}
 
 export const usePageTracking = () => {
   const [location] = useLocation();
@@ -10,6 +16,7 @@ export const usePageTracking = () => {
       page_path: location + search,
       page_search: search,
       page_hash: location,
+      screen_name: getKeyFromValue(location) ?? location,
     });
   }, [location, search]);
 };
